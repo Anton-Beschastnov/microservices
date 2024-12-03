6e674132b6e80a9c6ce09266089d371792f14318
Anton-Beschastnov <131583882+Anton-Beschastnov@users.noreply.github.com>
2024-12-04 00:12:50 +0300

    main. Add FeignClient in microservices

--- contacts_service/src/main/java/by/micro/contacts_service/service/ContactsServiceFeignClient.java
+++ contacts_service/src/main/java/by/micro/contacts_service/service/ContactsServiceFeignClient.java
@@ -1,5 +1,6 @@
 package by.micro.contacts_service.service;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,7 +11,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name = "PHONE-CODE-SERVICE")
 public interface ContactsServiceFeignClient {
     @GetMapping("/api/phone/code/{phoneCodeId}")
+    @CircuitBreaker(name = "phone_code_service", fallbackMethod = "getCodeInfoFallback")
     ResponseEntity<String> getCodeInfo(@PathVariable Integer phoneCodeId);
+
+    default ResponseEntity<String> getCodeInfoFallback( Integer phoneCodeId, Throwable exception){
+        System.out.println("phoneCodeId: " + phoneCodeId);
+        System.out.println("exception: " + exception.getMessage());
+        return ResponseEntity.ok("+7");
+    }
 
     @PostMapping("/api/phone/code/save")
     ResponseEntity<Integer> saveCode(@RequestParam String code,
