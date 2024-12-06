6ffa7a8daeeec0125f2fc0616254910668940ca5
Anton-Beschastnov <131583882+Anton-Beschastnov@users.noreply.github.com>
2024-12-07 01:21:06 +0300

    main. Add KeyCloak in microservices

--- gateway_service/src/main/java/by/micro/gateway_service/config/SecurityConfig.java
+++ gateway_service/src/main/java/by/micro/gateway_service/config/SecurityConfig.java
@@ -1,54 +1,45 @@
 package by.micro.gateway_service.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
-import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import java.util.List;
-import java.util.stream.Stream;
+import reactor.core.publisher.Flux;
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+    @Value("${spring.security.oauth2.client.provider.keycloak.issuer-uri}")
+    private String ISSUER;
+
     @Bean
-    public SecurityWebFilterChain
-    springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(auth -> auth
-                .pathMatchers("/auth/token").permitAll()
-                .pathMatchers("/api/contact/save").hasRole("ROLE_ADMIN")
-                .anyExchange().authenticated())
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeExchange()
+            .anyExchange().authenticated()
+            .and()
             .oauth2ResourceServer(oauth2 -> oauth2
-                    .jwt(jwt ->
-                            jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+                    .jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())));
         return http.build();
     }
+
+
     @Bean
-    public ReactiveJwtAuthenticationConverterAdapter
-    jwtAuthenticationConverter() {
-        var jwtGrantedAuthoritiesConverter = new
-                JwtGrantedAuthoritiesConverter();
-        JwtAuthenticationConverter converter = new
-                JwtAuthenticationConverter();
-        converter.setPrincipalClaimName("preferred_username");
-        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
-            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
-            var roles = (List<String>)
-                    jwt.getClaimAsMap("realm_access").get("roles");
-            return Stream.concat(authorities.stream(),
-                    roles.stream()
-                         .map(role -> "ROLE_" + role)
-                         .map(SimpleGrantedAuthority::new)
-                         .map(GrantedAuthority.class::cast))
-                         .toList();
-        });
-        return new ReactiveJwtAuthenticationConverterAdapter(converter);
+    public ReactiveJwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        ReactiveJwtAuthenticationConverter converter = new ReactiveJwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> Flux.fromIterable(jwtGrantedAuthoritiesConverter.convert(jwt)));
+        return converter;
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        return NimbusReactiveJwtDecoder.withJwkSetUri(ISSUER).build();
     }
 }
--- gateway_service/src/main/java/by/micro/gateway_service/service/KeycloakService.java
+++ gateway_service/src/main/java/by/micro/gateway_service/service/KeycloakService.java
@@ -1,46 +1,46 @@
-package by.micro.gateway_service.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
-import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientResponseException;
-import org.springframework.web.server.ResponseStatusException;
-import reactor.core.publisher.Mono;
-
-import java.util.Map;
-
-@Service
-public class KeycloakService implements IdentityProviderService  {
-    @Autowired
-    private WebClient webClient;
-    @Autowired
-    private ReactiveClientRegistrationRepository
-            clientRegistrationRepository;
-    @Override
-    public Mono<Map<String, Object>> getToken(String login, String password)
-    {
-        return clientRegistrationRepository.findByRegistrationId("keycloak")
-                                           .flatMap(clientRegistration -> webClient.post()
-                                                                                   .uri(clientRegistration.getProviderDetails().getTokenUri())
-                                                                                   .body(BodyInserters.fromFormData(OAuth2ParameterNames.GRANT_TYPE,
-                                                                                           clientRegistration.getAuthorizationGrantType().getValue())
-                                                                                                      .with(OAuth2ParameterNames.CLIENT_ID,
-                                                                                                              clientRegistration.getClientId())
-                                                                                                      .with(OAuth2ParameterNames.CLIENT_SECRET,
-                                                                                                              clientRegistration.getClientSecret())
-                                                                                                      .with(OAuth2ParameterNames.USERNAME, login)
-                                                                                                      .with(OAuth2ParameterNames.PASSWORD,
-                                                                                                              password))
-                                                                                   .retrieve()
-                                                                                   .bodyToMono(new
-                                                                                                       ParameterizedTypeReference<Map<String, Object>>() {
-                                                                                                       })
-                                                                                   .onErrorResume(WebClientResponseException.class, e
-                                                                                           > Mono.error(
-                                                                                           new
-                                                                                                   ResponseStatusException(e.getStatusCode(), e.getResponseBodyAsString()))));
-    }
-}
+//package by.micro.gateway_service.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.core.ParameterizedTypeReference;
+//import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.reactive.function.BodyInserters;
+//import org.springframework.web.reactive.function.client.WebClient;
+//import org.springframework.web.reactive.function.client.WebClientResponseException;
+//import org.springframework.web.server.ResponseStatusException;
+//import reactor.core.publisher.Mono;
+//
+//import java.util.Map;
+//
+//@Service
+//public class KeycloakService implements IdentityProviderService  {
+//    @Autowired
+//    private WebClient webClient;
+//    @Autowired
+//    private ReactiveClientRegistrationRepository
+//            clientRegistrationRepository;
+//    @Override
+//    public Mono<Map<String, Object>> getToken(String login, String password)
+//    {
+//        return clientRegistrationRepository.findByRegistrationId("keycloak")
+//                                           .flatMap(clientRegistration -> webClient.post()
+//                                                                                   .uri(clientRegistration.getProviderDetails().getTokenUri())
+//                                                                                   .body(BodyInserters.fromFormData(OAuth2ParameterNames.GRANT_TYPE,
+//                                                                                           clientRegistration.getAuthorizationGrantType().getValue())
+//                                                                                                      .with(OAuth2ParameterNames.CLIENT_ID,
+//                                                                                                              clientRegistration.getClientId())
+//                                                                                                      .with(OAuth2ParameterNames.CLIENT_SECRET,
+//                                                                                                              clientRegistration.getClientSecret())
+//                                                                                                      .with(OAuth2ParameterNames.USERNAME, login)
+//                                                                                                      .with(OAuth2ParameterNames.PASSWORD,
+//                                                                                                              password))
+//                                                                                   .retrieve()
+//                                                                                   .bodyToMono(new
+//                                                                                                       ParameterizedTypeReference<Map<String, Object>>() {
+//                                                                                                       })
+//                                                                                   .onErrorResume(WebClientResponseException.class, e
+//                                                                                           > Mono.error(
+//                                                                                           new
+//                                                                                                   ResponseStatusException(e.getStatusCode(), e.getResponseBodyAsString()))));
+//    }
+//}
