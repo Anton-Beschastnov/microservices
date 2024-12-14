ef19b5f57306d391080196e2b907ea5d651109de
Anton-Beschastnov <131583882+Anton-Beschastnov@users.noreply.github.com>
2024-12-14 19:04:47 +0300

    main. Add Config Server in microservices

--- mvc_oauth2_client_service/src/main/java/by/micro/mvc_oauth2_client_service/MvcOauth2ClientServiceApplication.java
+++ mvc_oauth2_client_service/src/main/java/by/micro/mvc_oauth2_client_service/MvcOauth2ClientServiceApplication.java
@@ -1,11 +1,14 @@
 package by.micro.mvc_oauth2_client_service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MvcOauth2ClientServiceApplication {
+    public static String name;
+
     public static void main(String[] args) {
         SpringApplication.run(MvcOauth2ClientServiceApplication.class, args);
     }
